84ffa991c793f9866a0d07cf456d1a00d569c42d
Felix Clinthorne <felixclinthorne@gmail.com>
2025-05-31 12:39:24 -0400

    configure store for TS, almost forgot!

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Provider } from "react-redux";
-import store from './store';
+import { store } from './store';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-export default configureStore({
+export const store = configureStore({
     reducer: {}
 });
+
+export type AppStore = typeof store;
+// infer these so they update as more middleware-related logic is added.
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
